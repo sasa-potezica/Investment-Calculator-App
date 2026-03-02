4a647dd776d61305da463d49d0951bd6925568fc
sasa-potezica <sasapotezica@yahoo.com>
2026-03-02 19:12:02 +0100

    Fix broken styling for UserInput

--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -18,49 +18,57 @@ const UserInput = () => {
   }
 
   return (
-    <section className="input-group">
-      <p>
-        <label htmlFor="">Initial Investment</label>
-        <input
-          type="number"
-          required
-          value={userInput.initialInvestment}
-          onChange={(event) =>
-            handleChange("initialInvestment", event.target.value)
-          }
-        />
-      </p>
-      <p>
-        <label htmlFor="">Annual Investment</label>
-        <input
-          type="number"
-          required
-          value={userInput.annualInvestment}
-          onChange={(event) =>
-            handleChange("annualInvestment", event.target.value)
-          }
-        />
-      </p>
-      <p>
-        <label htmlFor="">Expected Return</label>
-        <input
-          type="number"
-          required
-          value={userInput.expectedReturn}
-          onChange={(event) =>
-            handleChange("expectedReturn", event.target.value)
-          }
-        />
-      </p>
-      <p>
-        <label htmlFor="">Duration</label>
-        <input
-          type="number"
-          required
-          value={userInput.duration}
-          onChange={(event) => handleChange("duration", event.target.value)}
-        />
-      </p>
+    <section id="user-input">
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Initial Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.initialInvestment}
+            onChange={(event) =>
+              handleChange("initialInvestment", event.target.value)
+            }
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Annual Investment</label>
+          <input
+            type="number"
+            required
+            value={userInput.annualInvestment}
+            onChange={(event) =>
+              handleChange("annualInvestment", event.target.value)
+            }
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Expected Return</label>
+          <input
+            type="number"
+            required
+            value={userInput.expectedReturn}
+            onChange={(event) =>
+              handleChange("expectedReturn", event.target.value)
+            }
+          />
+        </p>
+      </div>
+      <div className="input-group">
+        <p>
+          <label htmlFor="">Duration</label>
+          <input
+            type="number"
+            required
+            value={userInput.duration}
+            onChange={(event) => handleChange("duration", event.target.value)}
+          />
+        </p>
+      </div>
     </section>
   );
 };
