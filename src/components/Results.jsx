d7f00d55d607b97617fc69cd139ceec476866096
sasa-potezica <sasapotezica@yahoo.com>
2026-03-04 18:59:48 +0100

    Handle investment calculation logic

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import calculateInvestmentResults from "../util/investment";
 
 const Results = ({ userInput }) => {
-  return console.log(userInput);
+  return calculateInvestmentResults(userInput);
 };
 
 export default Results;
