17ba18a313cd9ab951b0ca0fd9ba7df93e99ac1f
sasa-potezica <sasapotezica@yahoo.com>
2026-03-05 23:04:38 +0100

    Update results table display content

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -4,6 +4,10 @@ import { renderToReadableStream } from "react-dom/server";
 
 const Results = ({ userInput }) => {
   const resultsData = calculateInvestmentResults(userInput);
+  const initialInvestment =
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
   return (
     <table id="result">
       <thead>
@@ -17,19 +21,18 @@ const Results = ({ userInput }) => {
       </thead>
       <tbody>
         {resultsData.map((yearData) => {
-          const totalInterestValue =
-            yearData.valueEndOfYear - yearData.annualInvestment * yearData.year;
-          const initialInvestment =
-            resultsData[0].valueEndOfYear -
-            resultsData[0].interest -
-            resultsData[0].annualInvestment;
+          const totalInterest =
+            yearData.valueEndOfYear -
+            yearData.annualInvestment * yearData.year -
+            initialInvestment;
+          const totalAmountInvested = yearData.valueEndOfYear - totalInterest;
           return (
             <tr key={yearData.year}>
               <td>{yearData.year}</td>
               <td>{formatter.format(yearData.valueEndOfYear)}</td>
               <td>{formatter.format(yearData.interest)}</td>
-              <td>{}</td>
-              <td>{}</td>
+              <td>{formatter.format(totalInterest)}</td>
+              <td>{formatter.format(totalAmountInvested)}</td>
             </tr>
           );
         })}
